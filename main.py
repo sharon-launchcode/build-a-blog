cada1adef4a9baa2c2e0ff61513d501a2e8894dd
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-15 20:47:06 -0500

    added sqlalchemy uri

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog-user@localhost:8889/build-a-blog'
 
 tasks = []
 
