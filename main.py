69d4ae4835bb8f30f0c6d138f590b10ea5472e1e
austin-2017 <sharon.austin.2017@gmail.com>
2018-01-15 20:50:26 -0500

    persistent databases

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog-user@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -15,4 +23,5 @@ def index():
 
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
